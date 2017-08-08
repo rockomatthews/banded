02dd31e5555fae5b82caf85b436fb16590f5fb6b
Rob Matthews <rob.matthews@me.com>
2017-08-08 03:18:47 -0500

    Finally fixed angular 4 to work with materialize

--- bandedAngularMobile/rockRadarFront/src/app/user.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    userName: string;
-    genre: string;
-    motto: string;
-    password: string;
-    confirmPassword: string;
-}
---
+++ rock-radar-front/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+declare var $:any;
+declare var jQuery:any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  private hideNavbar() {
+    $(".nav-extended").hide();
+  }
+}

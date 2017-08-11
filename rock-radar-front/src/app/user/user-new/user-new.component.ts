95cd8c2cf316f0798c4e4022fd88e2c38324c84f
Rob Matthews <rob.matthews@me.com>
2017-08-11 01:00:32 -0500

    Creating register fields and user attributes. need to next link genres selected to user

--- rock-radar-front/src/app/user/user-new/user-new.component.ts
+++ rock-radar-front/src/app/user/user-new/user-new.component.ts
@@ -1,3 +1,4 @@
+import { User } from "./../user";
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-new.component.css']
 })
 export class UserNewComponent implements OnInit {
-
+  newUser = new User();
   constructor() { }
 
   ngOnInit() {
--- rock-radar-front/src/app/user/user.component.ts
+++ rock-radar-front/src/app/user/user.component.ts
@@ -8,7 +8,11 @@ import { User } from './user';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  users: Array<User> = [];
+  users: Array<User> = [
+    new User(1, "suckboi", "if you can't a tiger by its tail don't fail", ["Alternative Rock", "Metal"], "Grandtour69!", "Grandtour69!"),
+    new User(2, "jimbo", "whatever", ["Metal"], "closetLoser!", "closetLoser!"),
+    new User(3, "trixster", "I love waking up but am hated by all", ["Pop", "R&B"], "Wuswuswus44!", "Wuswuswus44!")
+  ];
 
   constructor(private _userService: UserService) { }
 
